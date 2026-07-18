a1098db48113ac665b24d1c188e689a84b452846
sanikatavare08-pixel <sanikatavare08@gmail.com>
2026-07-18 18:32:07 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(
 )
 
 # ---------------- Load Model ----------------
-model = load_model("mask_final.tflite")
+model = load_model("mask_final.keras")
 
 # ---------------- Session State ----------------
 if "open_camera" not in st.session_state:
